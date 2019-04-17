052156fcc0e79fed86620c4173c6afb4687415db
Asenchik Oleg <olgasn@inbox.ru>
2019-04-17 22:32:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleADONET/DbInitializer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleADONET
+{
+    public static class DbInitializer
+    {
+        public static string Initialize(string connectionString)
+        {
+                                                  
+            int tanks_number = 75;
+            int fuels_number = 75;
+            int operations_number = 3000;
+            string tankType;
+            string tankMaterial;
+            float tankWeight;
+            float tankVolume;
+            string fuelType;
+            float fuelDensity;
+            string result = "";
+            Random randObj = new Random(1);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand check_Fuels = new SqlCommand("SELECT COUNT(*) FROM [Fuels]", connection);
+                int RecordsExist = (int)check_Fuels.ExecuteScalar();
+                if (RecordsExist > 0)
+                {
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    SqlCommand command = connection.CreateCommand();
+                    command.Transaction = transaction;
+                    try
+                    {
+                        //Заполнение таблицы емкостей
+                        string[] tank_voc = { "Цистерна_", "Ведро_", "Бак_", "Фляга_", "Цистерна_", "Стакан_" };//словарь названий емкостей
+                        string[] material_voc = { "Сталь", "Платина", "Алюминий", "ПЭТ", "Чугун", "Алюминий", "Золото", "Дерево", "Керамика" };//словарь названий видов топлива
+                        int count_tank_voc = tank_voc.GetLength(0);
+                        int count_material_voc = material_voc.GetLength(0);
+                        command.CommandText = "INSERT INTO Tanks (TankType, TankWeight, TankVolume, TankMaterial) VALUES";
+                        for (int tankID = 1; tankID <= tanks_number; tankID++)
+                        {
+                            tankType = tank_voc[randObj.Next(count_tank_voc)] + tankID.ToString();
+                            tankMaterial = material_voc[randObj.Next(count_material_voc)];
+                            tankWeight = 500 * (float)randObj.NextDouble();
+                            tankVolume = 200 * (float)randObj.NextDouble();
+                            command.CommandText = command.CommandText + "(" + tankType + ", " + tankWeight + ", " + tankVolume + ", " + tankMaterial + "), ";
+                        }
+                        command.CommandText = command.CommandText.TrimEnd(new Char[] { ',', ' ' }) + ";";
+                        //отправляет команду на вставку в базу данных
+                        command.ExecuteNonQuery();
+
+                        //Заполнение таблицы видов топлива
+                        string[] fuel_voc = { "Нефть_", "Бензин_", "Керосин_", "Мазут_", "Спирт_", "Водород_" };
+                        int count_fuel_voc = fuel_voc.GetLength(0);
+                        command.CommandText = "INSERT INTO Fuels (FuelType, FuelDensity) VALUES";
+                        for (int fuelID = 1; fuelID <= fuels_number; fuelID++)
+                        {
+                            fuelType = fuel_voc[randObj.Next(count_fuel_voc)] + fuelID.ToString();
+                            fuelDensity = 2 * (float)randObj.NextDouble();
+                            command.CommandText = command.CommandText + "(" + fuelType + ", " + fuelDensity + "), ";
+                        }
+                        command.CommandText = command.CommandText.TrimEnd(new Char[] { ',', ' ' }) + ";";
+                        //отправляет команду на вставку в базу данных
+                        command.ExecuteNonQuery();
+
+                        //Заполнение таблицы операций
+                        command.CommandText = "INSERT INTO Fuels (TankID, FuelID, Inc_Exp, Date) VALUES";
+                        for (int operationID = 1; operationID <= operations_number; operationID++)
+                        {
+                            int tankID = randObj.Next(1, tanks_number - 1);
+                            int fuelID = randObj.Next(1, fuels_number - 1);
+                            int inc_exp = randObj.Next(200) - 100;
+                            DateTime today = DateTime.Now.Date;
+                            DateTime operationdate = today.AddDays(-operationID);
+                            command.CommandText = command.CommandText + "(" + tankID + ", " + fuelID + ", " + inc_exp + ", " + operationdate + "), ";
+                        }
+                        command.CommandText = command.CommandText.TrimEnd(new Char[] { ',', ' ' }) + ";";
+                        //отправляет команду на вставку в базу данных
+                        command.ExecuteNonQuery();
+
+                        // подтверждаем транзакцию
+                        transaction.Commit();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        result = ex.Message;
+                        transaction.Rollback();
+                    }
+
+                }
+            }
+            return result;
+
+        }
+    }
+
+}
---
+++ ConsoleADONET/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleADONET
+{
+    class Program
+    {
+        public static object ConfigurationManager { get; private set; }
+
+        static void Main(string[] args)
+        {
+            //Считывание строки подключения из конфигурационного файла
+            string сonnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["toplivoConnectionString"].ConnectionString;
+            //Инициализация базы данных
+            string InitializeResult = DbInitializer.Initialize(сonnectionString);
+            //Открытие соединения для выполнения команд
+            using (SqlConnection connection = new SqlConnection(сonnectionString))
+            {
+                connection.Open();
+
+                Console.WriteLine("====== Будет выполнена выборка данных (нажмите любую клавишу) ========");
+                Console.ReadKey();
+                Print(Select1(connection));
+                Console.WriteLine("====== Будет выполнена выборка данных (нажмите любую клавишу) ========");
+                Console.ReadKey();
+                Print(Select2(connection));
+                Console.WriteLine("====== Будет выполнена выборка данных (нажмите любую клавишу) ========");
+                Console.ReadKey();
+                Print(Select3(connection));
+
+            }
+            }
+
+        static void Print(IList items)
+        {
+            Console.WriteLine("Записи: ");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static string Insert(SqlConnection connection)
+        {
+            string message = "";
+            SqlTransaction transaction = connection.BeginTransaction();
+            SqlCommand command = connection.CreateCommand();
+            command.Transaction = transaction;
+            try
+            {
+
+                command.CommandText = "";
+                //отправляет команду в базу данных
+                command.ExecuteNonQuery();
+
+                // подтверждаем транзакцию
+                transaction.Commit();
+                message = "";
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                transaction.Rollback();
+                message = "";
+
+            }
+            return message;
+ 
+        }
+
+        static string Delete(SqlConnection connection)
+        {
+            string message = "";
+            //todo
+            return message;
+
+        }
+        static IList Select1(SqlConnection connection)
+        {
+            List<string> results= new List<string>();
+            //todo
+
+            return results;
+        }
+        static IList Select2(SqlConnection connection)
+        {
+            List<string> results = new List<string>();
+            //todo
+
+            return results;
+        }
+        static IList Select3(SqlConnection connection)
+        {
+            List<string> results = new List<string>();
+            //todo
+
+            return results;
+        }
+
+        static string Update(SqlConnection connection)
+        {
+            string message = "";
+
+            //todo
+
+            return message;
+
+        }
+
+    }
+    }
+
